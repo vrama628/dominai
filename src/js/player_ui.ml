d8f3f71a8e9263d2e01af1f13cbddafdbe49370d
Vijay Ramamurthy <vrama628@gmail.com>
2022-10-04 13:02:00 -0400

    WIP

--- src/js/player_ui.ml
+++ src/js/player_ui.ml
@@ -3,14 +3,47 @@ open Js_of_ocaml
 (* open Js_of_ocaml_lwt *)
 open Js_of_ocaml_tyxml.Tyxml_js
 
-(* open React *)
+open React
+
 (* open ReactiveData *)
 open Base
 (* open Dominai *)
 
+let websocket ~(game_key : string) : Jsonrpc.Packet.t event =
+  let websocket_js =
+    new%js WebSockets.webSocket
+      (Printf.sprintf "/game/%s/join" game_key |> Js.string)
+  in
+  let event, trigger = E.create () in
+  let on_message (event : 'a WebSockets.messageEvent Js.t) : bool Js.t =
+    trigger
+    @@ Jsonrpc.Packet.t_of_yojson
+    @@ Yojson.Safe.from_string
+    @@ Js.to_string
+    @@ event##.data;
+    Js._false
+  in
+  websocket_js##.onmessage := Dom.handler on_message;
+  event
+
+type state = { in_play : bool }
+
+let initial_state : state = { in_play = false }
+
+let state_to_string (state : state) : string =
+  ignore state;
+  ""
+
+let state_s (event : Jsonrpc.Packet.t event) : state signal =
+  let state_s, set_state = S.create initial_state in
+  let update = function _ -> ignore set_state in
+  ignore (E.map update event);
+  state_s
+
 let game_state_app ~(game_key : string) : Html_types.div Html.elt =
   let open Html in
-  div [txt game_key]
+  let state_s = state_s (websocket ~game_key) in
+  div [txt game_key; R.Html.txt (S.map state_to_string state_s)]
 
 let main () : unit Lwt.t =
   let app_container = Dom_html.getElementById_exn "app" in
